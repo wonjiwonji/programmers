9bb04b2bf6aed0a25fdd6176d8bfc4873a1926ba
wonjiwonji <122261115+wonjiwonji@users.noreply.github.com>
2024-02-01 14:25:58 +0900

    [level 0] Title: 문자열 섞기, Time: 11.71 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        String a[] = str1.split("");
+        String b[] = str2.split("");
+        
+        for(int i=0; i<str1.length(); i++) {
+            answer += a[i]+b[i];
+        }
+        return answer;
+    }
+}
